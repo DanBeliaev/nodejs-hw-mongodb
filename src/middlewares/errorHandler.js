09f703db5f30f7c48a260a5cecb7503bd2385223
DanBeliaev <beliaev1108@gmail.com>
2025-02-27 00:49:00 +0000

    fin

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,19 @@
+import { isHttpError } from "http-errors";
+
+export const errorHandler = (err, req, res, next) => {
+
+    if (isHttpError(err) === true) {
+        return res
+            .status(err.statusCode)
+            .json({
+                status: err.statusCode,
+                message: err.message
+            });
+    }
+
+    res.status(500).json({
+        status: 500,
+        message: "Something went wrong",
+        data: err.message
+    });
+};
