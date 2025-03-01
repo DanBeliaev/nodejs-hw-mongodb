f958e30db5fa9d25f07168e3c56c73b8575d8fd4
DanBeliaev <beliaev1108@gmail.com>
2025-03-01 17:18:35 +0000

    fin12

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 import express from "express";
+=======
+import {Router} from "express";
+>>>>>>> Stashed changes
 
 import {
     getContactsController,
@@ -12,8 +16,15 @@ import { validateBody } from "../middlewares/validateBody.js";
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 import { isValidId } from "../middlewares/isValidId.js";
 
+<<<<<<< Updated upstream
 const jsonParser = express.json();
 const router = express.Router();
+=======
+
+const router = Router();
+
+router.use(authenticate);
+>>>>>>> Stashed changes
 
 router.get(
     '/contacts',
@@ -25,15 +36,18 @@ router.get(
     ctrlWrapper(getContactByIdController)
 );
 router.post(
+<<<<<<< Updated upstream
     "/contacts",
     jsonParser,
+=======
+    "/",
+>>>>>>> Stashed changes
     validateBody(createContactSchema),
     ctrlWrapper(createContactsController)
 );
 router.patch(
     "/contacts/:contactId",
     isValidId,
-    jsonParser,
     validateBody(updateContactSchema),
     ctrlWrapper(patchContactController)
 );
