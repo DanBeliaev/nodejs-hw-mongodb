c772d06f282e12e8dee437116adbdd39a714e031
DanBeliaev <beliaev1108@gmail.com>
2025-04-13 22:54:30 +0100

    finishss

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
+import * as fs from "node:fs";
+import path from 'node:path';
 import express from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
 import cookieParser from "cookie-parser";
-
+import swaggerUi from 'swagger-ui-express';
+// import specs from '../swaggerConfig.js';
 import router from "./routers/index.js";
 import { env } from './utils/env.js';
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
@@ -10,6 +13,10 @@ import { errorHandler } from "./middlewares/errorHandler.js";
 import { UPLOAD_DIR } from "./constants/index.js";
 
 const PORT = Number(env('PORT', '3000'));
+
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.resolve('docs/swagger.json'), 'utf-8')
+);
 
 export const setupServer = () => {
     const app = express();
@@ -26,6 +33,9 @@ export const setupServer = () => {
         })
     );
     app.use('/uploads', express.static(UPLOAD_DIR));
+
+
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     app.use(router);
 
